004223f8b6c079fca9a4e2257d2e788d1e1c131e
PYamini-21 <112534113@kanchiuniv.ac.in>
2026-03-03 11:10:22 +0530

    Add files via upload

---
+++ six.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class six
+{
+static public void main(String[]args)throws Exception
+{
+Scanner input = new Scanner(System.in);
+int a,b,c;
+System.out.println("enter value for a");
+a = input.nextInt();
+System.out.println("enter value for b");
+b = input.nextInt();
+System.out.println("addition of two numbers");
+c=a+b;
+System.out.println(" "+c);
+System.out.println("substraction of two numbers");
+c=a-b;
+System.out.println(" "+c);
+System.out.println("multiplication of two numbers");
+c=a*b;
+System.out.println(" "+c);
+System.out.println("division of two numbers");
+c=a/b;
+System.out.println(" "+c);
+}}
