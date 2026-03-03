76943d1d7780f73079ebcbf2241e7ad6cdc9d6b9
PYamini-21 <112534113@kanchiuniv.ac.in>
2026-03-03 09:33:18 +0530

    Add files via upload

---
+++ five.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class five
+{
+static public void main(String[]args)throws Exception
+{
+String s1,s2;
+int i1,i2;
+scanner input=new scanner(System.in);
+System.out.println("enter the frist value");
+s1=input.nextLine();
+i1=Integer.passaInt(s1);
+System.out.println("enter second value");
+s2=input nextLine();
+i2=Integer.passaInt(s2);
+if((i1%i2)==0
+{
+System.out.println("the given number is even");
