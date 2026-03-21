d641cc6199a78783af144d7131d5f68d572523db
PYamini-21 <112534113@kanchiuniv.ac.in>
2026-03-21 10:55:00 +0530

    Add files via upload

---
+++ seven.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class seven
+{
+static public void main(String[]args)throws Exception
+{
+Scanner input = new Scanner(System.in);
+String s1,s2;
+System.out.println("enter first string");
+s1 = input.nextLine();
+System.out.println("enter second string");
+s2 = input.nextLine();
+if(s1.equals(s2))
+{
+System.out.println("TWO STRING ARE EQUAL");
+}
+else
+{ 
+System.out.println("TWO STRING ARE NOT EQUAL");
+}x
+}}
